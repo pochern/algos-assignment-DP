4ea703cbab912bce8291fef9327151014fc0ddcd
Polina Chernomaz <polina@crosscompute.com>
2019-04-03 00:57:13 -0400

    materials for DP assignment

--- GlassFalling.java
+++ GlassFalling.java
@@ -3,36 +3,92 @@
  */
 public class GlassFalling {
 
-  // Do not change the parameters!
-  public int glassFallingRecur(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
-  }
+	// Do not change the parameters!
+	public int glassFallingRecur(int floors, int sheets) {
+		// If there are no floors, there will be no trials. If there is one floor, there will be one trial.
+		if(floors == 1||floors == 0) {
+			return floors;
+		}
+		// If there is one sheet, it will have trial=floor
+		if(sheets == 1) {
+			return floors;		// have to go through all floors
+		}
+		int min = Integer.MAX_VALUE, temp;
+		for(int i=1; i<=floors; i++) {
+			temp = Math.max(glassFallingRecur(i-1, sheets-1), glassFallingRecur(floors-i, sheets));
+			min = Math.min(temp, min);
+		}
+		// + 1 because throwing a sheet counts as attempt
+		return min + 1;
+	}
 
-  // Optional:
-  // Pick whatever parameters you want to, just make sure to return an int.
-  public int glassFallingMemoized() {
-    // Fill in here and change the return
-    return 0;
-  }
+	//Optional:
+	//Pick whatever parameters you want to, just make sure to return an int.
+	public int glassFallingMemoized(int floors, int sheets) {
+		int[][] arr = new int[floors+1][sheets+1];
+		for(int i=0; i<floors; i++) {
+			for(int j=0; j<sheets; j++) {
+				arr[i][j] = Integer.MAX_VALUE;
+			}
+		}
+		return glassFallingMemoizedCont(floors, sheets, arr);
+	}
 
-  // Do not change the parameters!
-  public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
-  }
+	public int glassFallingMemoizedCont(int floors, int sheets, int[][] sheetDrops) {
+		//If there are no floors, there will be no trials. If there is one floor, there will be one trial.
+		if(floors == 1||floors == 0) {
+			return floors;
+		}
+
+		//If there is one sheet, it will have trial=floor
+		if(sheets == 1) {
+			return floors;	// have to go through all floors
+		}
+		int min = Integer.MAX_VALUE;
+		int temp;
+		for(int i=1; i<=floors; i++) {
+			temp = Math.max(glassFallingMemoizedCont(i-1, sheets-1, sheetDrops), glassFallingMemoizedCont(floors-i, sheets, sheetDrops));
+			min = Math.min(temp, min);
+		}
+
+		//+ 1 because throwing a sheet counts as attempt
+		return min + 1;
+	}
+
+	// Do not change the parameters!
+	public int glassFallingBottomUp(int floors, int sheets) {
+		int [][] sheetDrops = new int[floors+1][sheets+1];
+		for(int i=1; i<=sheets; i++) {
+			sheetDrops[0][i] = 0;
+			sheetDrops[1][i] = 1;
+		}
+		for(int i=1; i<=floors; i++) {
+			sheetDrops[i][1] = i;
+		}
+		for(int i=2; i<=sheets; i++) {
+			for(int j=2; j<=floors; j++) {
+				sheetDrops[j][i] = Integer.MAX_VALUE;
+				int temp;
+				for(int k=1; k<=j; k++) {
+					temp = 1 + Math.max(sheetDrops[k-1][i-1], sheetDrops[j-k][i]);
+					sheetDrops[j][i] = Math.min(temp, sheetDrops[j][i]);
+				}
+			}
+		}
+		return sheetDrops[floors][sheets];
+	}
 
 
-  public static void main(String args[]){
-      GlassFalling gf = new GlassFalling();
+	public static void main(String args[]){
+		GlassFalling gf = new GlassFalling();
 
-      // Do not touch the below lines of code, and make sure
-      // in your final turned-in copy, these are the only things printed
-      int minTrials1Recur = gf.glassFallingRecur(27, 2);
-      int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
-      int minTrials2Recur = gf.glassFallingRecur(100, 3);
-      int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
-      System.out.println(minTrials1Recur + " " + minTrials1Bottom);
-      System.out.println(minTrials2Recur + " " + minTrials2Bottom);
-  }
+		// Do not touch the below lines of code, and make sure
+		// in your final turned-in copy, these are the only things printed
+		int minTrials1Recur = gf.glassFallingRecur(27, 2);
+		int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
+		int minTrials2Memo = gf.glassFallingMemoized(100, 3);
+		int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
+		System.out.println(minTrials1Recur + " " + minTrials1Bottom);
+		System.out.println(minTrials2Memo + " " + minTrials2Bottom);
+	}
 }
--- RodCutting.java
+++ RodCutting.java
@@ -3,31 +3,54 @@
  */
 public class RodCutting {
 
-  // Do not change the parameters!
-  public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
+	// Do not change the parameters!
+	public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
+		if (rodLength <= 0) {
+			return 0;
+		}
 
-  // Do not change the parameters!
-  public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
-  }
+		int maximum = Integer.MIN_VALUE;
 
 
-  public static void main(String args[]){
-      RodCutting rc = new RodCutting();
+		for (int i = 0; i<rodLength; i++) {
+			maximum = Math.max(maximum, lengthPrices[i] + rodCuttingRecur(rodLength-i-1, lengthPrices));
+		}
 
-      // In your turned in copy, do not touch the below lines of code.
-      // Make sure below is your only output.
-      int length1 = 7;
-      int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
-      int length2 = 14;
-      int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
-      int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
-      int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
-      int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
-      int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
-      System.out.println(maxSell1Recur + " " + maxSell1Bottom);
-      System.out.println(maxSell2Recur + " " + maxSell2Bottom);
-  }
+		return maximum;
+	}
+
+	// Do not change the parameters!
+	public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
+		int arr[] = new int[rodLength+1];
+		arr[0] = 0;
+
+
+		for (int i = 1; i<=rodLength; i++)
+		{
+			int maximum = Integer.MIN_VALUE;
+			for (int j = 0; j < i; j++)
+				maximum = Math.max(maximum, lengthPrices[j] + arr[i-j-1]);
+			arr[i] = maximum;
+		}
+
+		return arr[rodLength];
+	}
+
+
+	public static void main(String args[]){
+		RodCutting rc = new RodCutting();
+
+		// In your turned in copy, do not touch the below lines of code.
+		// Make sure below is your only output.
+		int length1 = 7;
+		int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
+		int length2 = 14;
+		int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
+		int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
+		int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+		int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
+		int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
+		System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+		System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+	}
 }
